c6d5d8e82811308f2c908b737e4ae13713291443
offline171 <offline.stein@gmail.com>
2025-06-28 23:37:37 -0400

    Added more functionality to cartRouter
    Now that I have a baseline for the table in postgreSQL, I have a better ideal on how to actually get the data I want from these functions. Notably, despite the cartItems table only containing the id of the row, as well as the foreign keys of both the user and item associated, there's still a way to figure out the name of the item through the fetch item function. I should definitely export and require it later but this is easier for now.

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -8,27 +8,46 @@ cartRouter.get("/", async function(req,res){
 
 cartRouter.get("/:cartId", async function(req,res){
     const cartItem = (await fetchCartItem(req.params.cartId));
-    res.send(`GET HTTP methed on cart item/${cartItem.name} resource`);
+    const item = (await fetchItem(cartItem.itemId));
+    res.send(`GET HTTP methed on cart item/${item.name} resource`);
 });
 
 cartRouter.put("/:cartId", async function(req,res){
     const cartItem = (await fetchCartItem(req.params.cartId));
-    res.send(`PUT HTTP methed on cart item/${cartItem.name} resource`);
+    const item = (await fetchItem(cartItem.itemId));
+    res.send(`PUT HTTP methed on cart item/${item.name} resource`);
 });
 
 cartRouter.post("/:cartId", async function(req,res){
     const cartItem = (await fetchCartItem(req.params.cartId));
-    res.send(`POST HTTP methed on cart item/${cartItem.name} resource`);
+    const item = (await fetchItem(cartItem.itemId));
+    res.send(`POST HTTP methed on cart item/${item.name} resource`);
 });
 
 cartRouter.delete("/:cartId", async function(req,res){
     const cartItem = (await fetchCartItem(req.params.cartId));
-    res.send(`DELETE HTTP methed on cart item/${cartItem.name} resource`);
+    const item = (await fetchItem(cartItem.itemId));
+    res.send(`DELETE HTTP methed on cart item/${item.name} resource`);
 });
 
 async function fetchCartItem(cartItemId){
   try{
-    const { rows } = await pool.query("SELECT * FROM items WHERE id = $1", [cartItemId]);
+    const { rows } = await pool.query("SELECT * FROM cartitems WHERE id = $1", [cartItemId]);
+    const items = rows[0];
+    if(items) {
+      return items;
+    } else {
+      console.log('Item not found');
+    }
+  } catch(error) {
+    console.error('Error, cannot find items.');
+  }
+}
+
+//get this from export and require later
+async function fetchItem(itemsId){
+  try{
+    const { rows } = await pool.query("SELECT * FROM items WHERE id = $1", [itemsId]);
     const items = rows[0];
     if(items) {
       return items;
